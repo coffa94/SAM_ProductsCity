7777e307ea8919f186bbea0bd8e586240fe0e07a
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-17 16:39:00 +0200

    Updating data structure DataNegozioJSon
    - aggiunti getter, setter e costruttori
    - per errore nella conversione con il pacchetto GSON ho rinominato le
    variabili mettendole senza maiuscole (FARE ATTENZIONE)

--- app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/DataNegozioJSon.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/DataNegozioJSon.java
@@ -3,10 +3,31 @@ package com.gmail.davidecoffaro.productscity.utilclass;
 import java.util.ArrayList;
 
 public class DataNegozioJSon {
-    private ArrayList<Prodotto> listaProdotti;
-    private String mailRider;
+    private ArrayList<Prodotto> listaprodotti;
+    private String mailrider;
 
     public DataNegozioJSon(){
+        
+    }
 
+    public DataNegozioJSon(ArrayList<Prodotto> inputListaProdotti, String inputMailRider){
+        listaprodotti = inputListaProdotti;
+        mailrider = inputMailRider;
+    }
+
+    public ArrayList<Prodotto> getListaprodotti() {
+        return listaprodotti;
+    }
+
+    public void setListaprodotti(ArrayList<Prodotto> listaprodotti) {
+        this.listaprodotti = listaprodotti;
+    }
+
+    public String getMailrider() {
+        return mailrider;
+    }
+
+    public void setMailrider(String mailrider) {
+        this.mailrider = mailrider;
     }
 }
