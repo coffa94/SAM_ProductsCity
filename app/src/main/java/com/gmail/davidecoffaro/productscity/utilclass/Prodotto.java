e4abbb9a21d05e4cf13fa8f3fbab1872d626785d
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-19 00:23:48 +0200

    Getter and setter of Prodotto, add descrizione

--- app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/Prodotto.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/Prodotto.java
@@ -4,24 +4,51 @@ import java.net.URL;
 
 public class Prodotto {
     private String nome;
+    private String descrizione;
     private float prezzo;
     private String immagine;
 
     public Prodotto(String nome, float prezzo){
         this.nome = nome;
+        this.descrizione = descrizione;
         this.prezzo = prezzo;
     }
 
-
-
-    public Prodotto(String nome, float prezzo, String immagine){
+    public Prodotto(String nome, String descrizione, float prezzo){
         this.nome = nome;
+        this.descrizione = descrizione;
         this.prezzo = prezzo;
+    }
+
+    public Prodotto(String nome, String descrizione, float prezzo, String immagine){
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.immagine = immagine;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setImmagine(String immagine) {
         this.immagine = immagine;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
     }
 
     public float getPrezzo() {
