69b9418fd2f7471f06193010c91c6b567734523b
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-20 01:38:44 +0200

    geocoder management
    - use of asynctask to not execute this code in UI thread
    - use of location.geocoder to translate latitude and longitude of
    position customer to his current address
    - on post execute write the result address to the views of
    InsertCustomerDataActivity

---
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/task/MyGeocodingTask.java
@@ -0,0 +1,62 @@
+package com.gmail.davidecoffaro.productscity.utilclass.task;
+
+import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.AsyncTask;
+import android.widget.EditText;
+
+import com.gmail.davidecoffaro.productscity.R;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.List;
+import java.util.Locale;
+
+public class MyGeocodingTask extends AsyncTask<Double, Void, Address> {
+    WeakReference<Activity> linkedActivity;
+
+    public MyGeocodingTask(Activity inputActivity){
+        linkedActivity = new WeakReference<Activity>(inputActivity);
+
+    }
+
+    @Override
+    protected Address doInBackground(Double... doubles) {
+        Geocoder geocoder = new Geocoder(linkedActivity.get(), new Locale("it"));
+
+        List<Address> list = null;
+        try {
+            list = geocoder.getFromLocation(doubles[0], doubles[1],1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(list!=null){
+            return list.get(0);
+        }else{
+            return null;
+        }
+
+    }
+
+    @Override
+    protected void onPostExecute(final Address address) {
+        super.onPostExecute(address);
+
+        if(address!=null){
+            linkedActivity.get().runOnUiThread(new Runnable(){
+                @Override
+                public void run() {
+                    ((EditText)linkedActivity.get().findViewById(R.id.editTextAddress)).setText(address.getAddressLine(0));
+                    ((EditText)linkedActivity.get().findViewById(R.id.editTextCity)).setText(address.getLocality());
+                    ((EditText)linkedActivity.get().findViewById(R.id.editTextCap)).setText(address.getPostalCode());
+                    ((EditText)linkedActivity.get().findViewById(R.id.editTextProvince)).setText(address.getSubAdminArea());
+
+                }
+            });
+        }
+
+
+    }
+}
