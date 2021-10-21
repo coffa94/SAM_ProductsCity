1b87653061b247012811637909c316ef002d1610
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-21 23:29:44 +0200

    Preparazione entity (tabella) del database Negozio
    - aggiunte proprietà delle singole righe della tabella
    - aggiunta variabile quantita alla classe Prodotto per gestire la
    tabella correttamente

--- app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/Prodotto.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/utilclass/Prodotto.java
@@ -1,31 +1,46 @@
 package com.gmail.davidecoffaro.productscity.utilclass;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.net.URL;
 
+@Entity
 public class Prodotto {
+    @PrimaryKey(autoGenerate = true)
+    public int productid;
+    @ColumnInfo(name="nome")
     private String nome;
+    @ColumnInfo(name="descrizione")
     private String descrizione;
+    @ColumnInfo(name="prezzo")
     private float prezzo;
     private String immagine;
+    @ColumnInfo(name="quantita")
+    private int quantita;
 
-    public Prodotto(String nome, float prezzo){
+    /*public Prodotto(String nome, float prezzo){
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
-    }
+        this.quantita=0;
+    }*/
 
     public Prodotto(String nome, String descrizione, float prezzo){
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
+        this.quantita=0;
     }
-
+    /*
     public Prodotto(String nome, String descrizione, float prezzo, String immagine){
         this.nome = nome;
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.immagine = immagine;
-    }
+        this.quantita=0;
+    }*/
 
     public void setNome(String nome) {
         this.nome = nome;
@@ -43,6 +58,10 @@ public class Prodotto {
         this.immagine = immagine;
     }
 
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -58,4 +77,9 @@ public class Prodotto {
     public String getImmagine() {
         return immagine;
     }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
 }
