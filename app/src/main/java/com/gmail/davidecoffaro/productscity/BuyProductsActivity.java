cdc47770525565449168df00654da1ba3d86c0ae
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-21 23:48:42 +0200

    utilizzo database jetpack room database
    - creazione del database NegozioDatabase contenente la tabella Prodotto
    con tutti i prodotti che è possibile acquistare; gestione delle righe
    del recyclerView con un adapter di tipo List<Prodotto> ottenuto tramite
    query di selezione sul database (query getAll). Inizialmente vengono
    eliminate tutte le righe delle tabelle del database, poi vengono
    inseriti i prodotti a partire dal file negozio1.json presente nella
    directory dell'app, e poi viene lanciata la query per ottenere i
    prodotti da visualizzare nel recycler view: tutto questo è fatto tramite
     asyncTask (DatabaseTask) e alcuni metodi in BuyProductsActivity
    utilizzati per coordinare thread UI con thread in background

--- app/src/main/java/com/gmail/davidecoffaro/productscity/BuyProductsActivity.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/BuyProductsActivity.java
@@ -3,13 +3,18 @@ package com.gmail.davidecoffaro.productscity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.gmail.davidecoffaro.productscity.utilclass.DataNegozioJSon;
 import com.gmail.davidecoffaro.productscity.utilclass.Prodotto;
 import com.gmail.davidecoffaro.productscity.utilclass.RVBuyProductListAdapter;
+import com.gmail.davidecoffaro.productscity.utilclass.databaseclass.NegozioDatabase;
+import com.gmail.davidecoffaro.productscity.utilclass.task.DatabaseTask;
+import com.gmail.davidecoffaro.productscity.utilclass.task.InsertAllDatabaseTask;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,7 +22,11 @@ import java.util.List;
 public class BuyProductsActivity extends AppCompatActivity {
     TextView totalOrder;
     RecyclerView rv;
+    RVBuyProductListAdapter adapter;
     Button next;
+    DataNegozioJSon negozioJSon;
+    NegozioDatabase db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +36,43 @@ public class BuyProductsActivity extends AppCompatActivity {
         totalOrder = (TextView) findViewById(R.id.textViewTotalOrder);
         next = (Button) findViewById(R.id.buttonNext);
 
-        rv = (RecyclerView) findViewById(R.id.recyclerViewBuyProductList);
+        //getting information shop from file negozio1.json
+        negozioJSon = new DataNegozioJSon(this);
+        negozioJSon.getDataNegozioJSon();
 
+        //creation of database with table Prodotto, using abstraction provided by Room Database
+        // table and rows of a database are linked to a java object
+        db = Room.databaseBuilder(getApplicationContext(),
+                NegozioDatabase.class, "database-negozio1").addMigrations(NegozioDatabase.MIGRATION_1_2).build();
+
+        DatabaseTask deleteAll = new DatabaseTask(this, "DeleteAll");
+        deleteAll.execute(new Prodotto[0]);
+
+
+        //initialize recyclerView without setting the adapter, it will be setted when the asyncTask
+        // database finishes the query execution
+        rv = (RecyclerView) findViewById(R.id.recyclerViewBuyProductList);
         rv.setLayoutManager(new LinearLayoutManager(this));
 
-        List<Prodotto> listaProdotti = new ArrayList<Prodotto>();
-        listaProdotti.add(new Prodotto("prodotto1", 35f));
-        listaProdotti.add(new Prodotto("prodotto2", 2f));
-        listaProdotti.add(new Prodotto("prodotto3", 14f));
+    }
 
-        RVBuyProductListAdapter adapter = new RVBuyProductListAdapter(listaProdotti);
+    public void insertAllProducts(){
+        Prodotto[] paramProdottoDatabase = new Prodotto[negozioJSon.getListaprodotti().size()];
+        paramProdottoDatabase = negozioJSon.getListaprodotti().toArray(paramProdottoDatabase);
+
+        //execute query insert all in a different thread with asyncTask
+        DatabaseTask insertAll = new DatabaseTask(this, "InsertAll");
+        insertAll.execute(paramProdottoDatabase);
+    }
+
+    public void getAllProducts(){
+        DatabaseTask getAll = new DatabaseTask(this, "GetAll");
+        getAll.execute(new Prodotto[0]);
+    }
+
+    public void updateRecyclerViewAdapter(List<Prodotto> listaProdotti){
+        adapter = new RVBuyProductListAdapter(listaProdotti);
 
         rv.setAdapter(adapter);
-
     }
 }
