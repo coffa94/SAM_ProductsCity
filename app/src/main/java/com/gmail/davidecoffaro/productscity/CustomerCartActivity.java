0ebd50f3c9001214277232560212b062e7a81138
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-22 16:21:43 +0200

    CustomerCart update recycler view
    - on create get the reference to NegozioDatabase and start AsyncTask to
    execute query getBuyed (get prodotto with quantita>0)
    - when getBuyed AsyncTask is finished, update the adapter of the
    recycler view with the listaProdotti from query getBuyed
    -updating UI of CustomerCartActivity

--- app/src/main/java/com/gmail/davidecoffaro/productscity/CustomerCartActivity.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/CustomerCartActivity.java
@@ -8,10 +8,13 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.room.Room;
 
 import com.gmail.davidecoffaro.productscity.utilclass.Prodotto;
 import com.gmail.davidecoffaro.productscity.utilclass.RVBuyProductListAdapter;
 import com.gmail.davidecoffaro.productscity.utilclass.RVCartProductListAdapter;
+import com.gmail.davidecoffaro.productscity.utilclass.databaseclass.NegozioDatabase;
+import com.gmail.davidecoffaro.productscity.utilclass.task.DatabaseTask;
 
 import org.w3c.dom.Text;
 
@@ -22,16 +25,19 @@ public class CustomerCartActivity extends AppCompatActivity implements View.OnCl
     TextView totalArticles;
     TextView totalOrder;
     RecyclerView rv;
+    RVCartProductListAdapter adapter;
     TextView nameSurnameCustomer;
     TextView completeAddressCustomer;
     Button modify;
     Button back;
     Button confirm;
 
+    NegozioDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buy_products);
+        setContentView(R.layout.activity_customer_cart);
 
         totalArticles = (TextView) findViewById(R.id.textViewTotalArticlesCartSummary);
         totalOrder = (TextView) findViewById(R.id.textViewTotalOrderCartSummary);
@@ -44,18 +50,19 @@ public class CustomerCartActivity extends AppCompatActivity implements View.OnCl
         back.setOnClickListener(this);
         confirm = (Button) findViewById(R.id.buttonConfirm);
         confirm.setOnClickListener(this);
+
+        //creation of database with table Prodotto, using abstraction provided by Room Database
+        // table and rows of a database are linked to a java object
+        db = Room.databaseBuilder(getApplicationContext(),
+                NegozioDatabase.class, "database-negozio1").addMigrations(NegozioDatabase.MIGRATION_1_2).build();
+
+        //initialization of recyclerView
         rv = (RecyclerView) findViewById(R.id.recyclerViewCartProductList);
 
         rv.setLayoutManager(new LinearLayoutManager(this));
 
-        List<Prodotto> listaProdotti = new ArrayList<Prodotto>();
-        listaProdotti.add(new Prodotto("prodotto1", 35f));
-        listaProdotti.add(new Prodotto("prodotto2", 2f));
-        listaProdotti.add(new Prodotto("prodotto3", 14f));
-
-        RVCartProductListAdapter adapter = new RVCartProductListAdapter(listaProdotti);
-
-        rv.setAdapter(adapter);
+        DatabaseTask getBuyed = new DatabaseTask(this, "GetBuyed");
+        getBuyed.execute(new Prodotto[0]);
 
     }
 
@@ -70,5 +77,11 @@ public class CustomerCartActivity extends AppCompatActivity implements View.OnCl
         if(v==confirm){
             //TODO click button confirm
         }
+    }
+
+    public void updateRecyclerViewAdapter(List<Prodotto> listaProdotti){
+        adapter = new RVCartProductListAdapter(listaProdotti);
+
+        rv.setAdapter(adapter);
     }
 }
