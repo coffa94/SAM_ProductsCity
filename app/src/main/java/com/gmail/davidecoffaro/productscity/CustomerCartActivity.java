455bb283020d7e552783c3aba7c374e692a2c29b
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-23 02:24:42 +0200

    update on CustomerCartActivity
    - get mailRider from intent from previous activity BuyProductsActivity
    - onStart get the sharedPreferences and set nameCustomer and his address
     in the correct view, save phoneCustomer in a variable
    - management of onClick method, modify start activity
    InsertCustomerDataActivity setting extra data LabelInfoCliente in
    intent, back finishes this activity and confirm prepare the intent with
     mail info to send as ACTION_SEND implicit intent
    - the listaProdotti set as list in the adapter Cart Recycler View is
    also saved in an arrayList to use it to create the email body
    - after getting listaProdotti to set the list in the recyclerView,
    execute async tasks of GetTotalDatabaseTask to get total articles and
    total order and set these info in the correct view

--- app/src/main/java/com/gmail/davidecoffaro/productscity/CustomerCartActivity.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/CustomerCartActivity.java
@@ -1,5 +1,9 @@
 package com.gmail.davidecoffaro.productscity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,6 +19,7 @@ import com.gmail.davidecoffaro.productscity.utilclass.RVBuyProductListAdapter;
 import com.gmail.davidecoffaro.productscity.utilclass.RVCartProductListAdapter;
 import com.gmail.davidecoffaro.productscity.utilclass.databaseclass.NegozioDatabase;
 import com.gmail.davidecoffaro.productscity.utilclass.task.DatabaseTask;
+import com.gmail.davidecoffaro.productscity.utilclass.task.GetTotalDatabaseTask;
 
 import org.w3c.dom.Text;
 
@@ -28,11 +33,15 @@ public class CustomerCartActivity extends AppCompatActivity implements View.OnCl
     RVCartProductListAdapter adapter;
     TextView nameSurnameCustomer;
     TextView completeAddressCustomer;
+    String phoneNumberCustomer;
     Button modify;
     Button back;
     Button confirm;
 
     NegozioDatabase db;
+    ArrayList<Prodotto> listaProdotti;
+    String mailRider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,24 +73,96 @@ public class CustomerCartActivity extends AppCompatActivity implements View.OnCl
         DatabaseTask getBuyed = new DatabaseTask(this, "GetBuyed");
         getBuyed.execute(new Prodotto[0]);
 
+        //get mail rider in intent from BuyProductsActivity
+        Intent i = getIntent();
+        if(i.hasExtra("EXTRA_RIDER_MAIL")){
+            mailRider = i.getStringExtra("EXTRA_RIDER_MAIL");
+        }else{
+            mailRider = "";
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //get customer info from sharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.preferences_file), Context.MODE_PRIVATE);
+        //initialize views with customerData in sharedPreferences, if they are set
+        if (sharedPreferences.contains("NameCustomer")) {
+            nameSurnameCustomer.setText(sharedPreferences.getString(getString(R.string.preferences_name_customer), ""));
+            completeAddressCustomer.setText(sharedPreferences.getString(getString(R.string.preferences_address_customer), ""));
+            phoneNumberCustomer = sharedPreferences.getString(getString(R.string.preferences_phone_customer),"");
+        }
     }
 
     @Override
     public void onClick(View v) {
         if(v==modify){
             //TODO click button modify
+            Intent i = new Intent(this, InsertCustomerDataActivity.class);
+            i.putExtra("LabelInfoCliente", "Modifica dati cliente");
+            startActivity(i);
         }
         if(v==back){
             //TODO click button back
+            finish();
         }
         if(v==confirm){
             //TODO click button confirm
+
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setData(Uri.parse("mailto:"));
+            i.setType("text/plain");
+            i.putExtra(Intent.EXTRA_EMAIL, new String[] {mailRider});
+            i.putExtra(Intent.EXTRA_SUBJECT, "Ordine ProductsCity");
+
+            //body mail construction
+            StringBuilder bodyMail = new StringBuilder();
+            bodyMail.append("Ordine da consegnare.\n");
+            bodyMail.append("Informazioni cliente: \n");
+            bodyMail.append(nameSurnameCustomer.getText().toString() + "\n");
+            bodyMail.append(completeAddressCustomer.getText().toString() + "\n");
+            bodyMail.append(phoneNumberCustomer + "\n");
+            bodyMail.append("\n");
+            bodyMail.append("Articoli da consegnare: \n");
+
+            //set body of the mail with products selected from customer
+            for (Prodotto p : listaProdotti){
+                bodyMail.append("   - " + p.getNome() + ", " + p.getQuantita() + "\n");
+            }
+            bodyMail.append("\n");
+
+            bodyMail.append("Totale articoli: " + totalArticles.getText().toString() + "\n");
+            bodyMail.append("Totale ordine: € " + totalOrder.getText().toString() + "\n");
+
+            i.putExtra(Intent.EXTRA_TEXT, bodyMail.toString());
+            startActivity(i);
         }
     }
 
     public void updateRecyclerViewAdapter(List<Prodotto> listaProdotti){
+        this.listaProdotti = (ArrayList<Prodotto>)listaProdotti;
+
         adapter = new RVCartProductListAdapter(listaProdotti);
 
         rv.setAdapter(adapter);
+
+        GetTotalDatabaseTask getTotalArticles = new GetTotalDatabaseTask(this, "GetTotalArticles");
+        getTotalArticles.execute();
+    }
+
+    public void updateTotalOrder(float queryTotalOrder){
+        totalOrder.setText(String.valueOf(queryTotalOrder));
+
+
+    }
+
+    public void updateTotalArticles(int queryTotalArticles){
+        totalArticles.setText(String.valueOf(queryTotalArticles));
+
+        GetTotalDatabaseTask getTotalOrder = new GetTotalDatabaseTask(this, "GetTotalOrder");
+        getTotalOrder.execute();
     }
 }
