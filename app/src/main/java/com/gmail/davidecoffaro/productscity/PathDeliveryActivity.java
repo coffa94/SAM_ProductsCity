52ae4f4fedac83293dddf9c5812acc45bb2cc78f
Davide Coffaro <davidecoffaro@gmail.com>
2021-10-23 15:22:40 +0200

    intent to google maps
    - check address in editText and send an intent with this address as
    destination to googlemaps

--- app/src/main/java/com/gmail/davidecoffaro/productscity/PathDeliveryActivity.java
+++ app/src/main/java/com/gmail/davidecoffaro/productscity/PathDeliveryActivity.java
@@ -2,10 +2,13 @@ package com.gmail.davidecoffaro.productscity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class PathDeliveryActivity extends AppCompatActivity implements View.OnClickListener {
     EditText addressDelivery;
@@ -20,14 +23,28 @@ public class PathDeliveryActivity extends AppCompatActivity implements View.OnCl
         pathDelivery = (Button) findViewById(R.id.buttonPathDelivery);
 
         pathDelivery.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
         if(v==pathDelivery){
-            //TODO click on path delivery (calcola percorso)
-            // intent a google maps passando posizione attuale e posizione di consegna come extra
+            //click on path delivery (calcola percorso)
+            // intent a google maps passando posizione di consegna come destinazione
+            if(addressDelivery.getText().toString().equals("")){
+                Toast.makeText(this, R.string.error_addressdelivery,Toast.LENGTH_SHORT).show();
+            }else{
+                //set navigation from device's position to addressDelivery using google maps
+                computePath(addressDelivery.getText().toString());
 
+            }
         }
+    }
+
+    public void computePath(String addressCustomer){
+        Uri gmmIntentUri = Uri.parse("google.navigation:q=" + addressCustomer);
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        startActivity(mapIntent);
     }
 }
